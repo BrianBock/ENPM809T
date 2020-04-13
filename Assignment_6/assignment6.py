3bc821d6930bde0fd2982749525b70b57ebb972e
Yash Ajay Savle <55366328+yashsavle@users.noreply.github.com>
2020-04-13 11:16:19 -0400

    Add files via upload

---
+++ Assignment_6/assignment6.py
@@ -0,0 +1,31 @@
+
+
+import time
+import os
+import sys
+import cv2
+import glob
+import gripper
+import sodar
+import motorcontrol02 as drive
+
+mygripper=gripper.gripper()
+#mygripper.init()
+key_press="a"
+#dcycle = "a"
+while not key_press == "p":
+	dcycle=input("Enter d cycle of gripper")
+	try:
+		dcycle=float(dcycle)
+		mygripper.grip(dcycle)
+		time.sleep(1)
+	except ValueError:
+		if not dcycle == "p":
+			print("Enter a valid digit") 
+
+	key_press = input("Select driving mode:")
+	
+	if not key_press =="p":
+		drive.key_input(key_press)
+
+		#print("Distance: "+str(sodar.distance()))
---
+++ Assignment_6/motorcontrol02.py
@@ -0,0 +1,104 @@
+import RPi.GPIO as gpio
+import time
+
+
+def init():
+	gpio.setmode(gpio.BOARD)
+	gpio.setup(31,gpio.OUT) #IN1
+	gpio.setup(33,gpio.OUT) #IN2
+	gpio.setup(35,gpio.OUT) #IN3
+	gpio.setup(37,gpio.OUT) #IN4
+	#gpio.setup(36,False)
+def gameover():
+	#set all pins low
+	gpio.setup(31 , False)
+	gpio.setup(33,False)
+	gpio.setup(35,False)
+	gpio.setup(37,False)
+
+def forward(tf):
+	init()
+	#Left wheels
+	gpio.output(31,True)
+	gpio.output(33,False)
+	#Right Wheels
+	gpio.output(35,False)
+	gpio.output(37,True)
+	#Wait
+	time.sleep(tf)
+	#Send all pins low & cleanup
+	gameover()
+	gpio.cleanup()
+def reverse(tf):
+	init()
+	#Left wheels
+	gpio.output(31,False)
+	gpio.output(33,True)
+	#Right Wheels
+	gpio.output(35,True)
+	gpio.output(37,False)
+	#Wait
+	time.sleep(tf)
+	#Send all pins low & cleanup
+	gameover()
+	gpio.cleanup()
+
+def left(tf):
+	init()
+	#Left wheels
+	gpio.output(31,False)
+	gpio.output(33,True)
+	#Right Wheels
+	gpio.output(35,False)
+	gpio.output(37,True)
+	#Wait
+	time.sleep(tf)
+	#Send all pins low & cleanup
+	gameover()
+	gpio.cleanup()
+def right(tf):
+	init()
+	#Left wheels
+	gpio.output(31,True)
+	gpio.output(33,False)
+	#Right Wheels
+	gpio.output(35,True)
+	gpio.output(37,False)
+	#Wait
+	time.sleep(tf)
+	#Send all pins low & cleanup
+	gameover()
+	gpio.cleanup()
+
+
+
+
+#forward(2)
+#reverse(2)
+#left(2)
+#right(2)
+
+def key_input(event):
+	init()
+	print("Key: " ,event)
+	key_press = event
+	tf=1
+	if key_press.lower() == 'w':
+		forward(tf)
+	elif key_press.lower() == 's':
+		reverse(tf)
+	elif key_press.lower() == 'a':
+		left(tf)
+	elif key_press.lower() == 'd':
+		right(tf)
+	else:
+		print("Invalid")
+		#gameover()
+		#gpio.cleanup()
+
+#while True:
+#	key_press = input("Select driving mode:")
+#	if key_press =='p':
+#		break
+#	key_input(key_press)
+
