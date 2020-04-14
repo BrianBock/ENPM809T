e7b1d137a3284f8f142e9e665b68dfbd0b18a177
Yash <--global>
2020-04-14 18:31:05 -0400

    Added distance measurement

--- Assignment_6/assignment6.py
+++ Assignment_6/assignment6.py
@@ -15,25 +15,25 @@ from picamera import PiCamera
 
 today = time.strftime("%y%m%d-%H%M%S")
 print(today)
-os.system("sudo mkdir "+ today)
+#os.system("sudo mkdir "+ today)
 mygripper=gripper.gripper()
 #mygripper.init()
 key_press="a"
 #dcycle = "a"
 
 drive.init_pins()
-
+sodar.init_spins()
 camera = PiCamera()
-camera.resolution = (640,480)
+camera.resolution = (1280,720)
 camera.framerate= 40
-rawCapture = PiRGBArray(camera, size=(640,480))
+rawCapture = PiRGBArray(camera, size=(1280,720))
 time.sleep(0.1)
 
 fourcc=cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('teleop.avi',fourcc,10,(1280,720))
+out = cv2.VideoWriter('teleop.avi',fourcc,1,(1280,720))
 i=0
-#cd= /home/pi/ENPM809T/Assignment_6/timelapse
-
+cd='/home/pi/ENPM809T/Assignment_6/timelapse'
+os.chdir(cd)
 while not key_press == "p":
 	i=i+1
 	dcycle=input("Enter d cycle of gripper")
@@ -49,16 +49,23 @@ while not key_press == "p":
 	
 	if not key_press =="p":
 		drive.key_input(key_press)
-		image = "sudo raspistill -w 1280 -h 720 -hf -vf -o /home/pi/ENPM809T/Assignment_6/" + today + "/" + str(i) + ".jpg"
-		os.system(image)
+		#image = "sudo raspistill -w 1280 -h 720 -hf -vf -o /home/pi/ENPM809T/Assignment_6/" + today + "/" + str(i) + ".jpg"
+		#os.system(image)
+		camera.capture(rawCapture,format="bgr")
+		og = rawCapture.array
 		print("Saved Image: ",i)
 		time.sleep(0.1)
-
-		og= cv2.imread(image)
+		og=cv2.flip(og,0)
+		#og= cv2.imread(image)
 		dcstr="Duty Cycle: %0.1f %%" % dcycle
 		font = cv2.FONT_HERSHEY_SIMPLEX
-		cv2.putText(og, dcstr, (10, 40), font, 1, (255, 0, 0), 2, cv2.LINE_AA)
-			#print("Distance: "+str(sodar.distance()))
+		cv2.putText(og, dcstr, (10, 40), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+		cv2.imwrite(str(i)+".jpg",og)
+		#print("Distance: "+str(sodar.distance()))
+		dist=sodar.distance()
+		distr="Distance: %0.3f cm "% dist
+		cv2.putText(og, distr, (10, 80), font, 1, (0,255, 0), 2, cv2.LINE_AA)
+		rawCapture.truncate(0)
 		out.write(og)
 print("Clearing pins and exiting")
 drive.gameover()
